ac080978778755e12134dfb5d415e895ff507126
QuanChi2003 <109083423+QuanChi2003@users.noreply.github.com>
2025-10-05 10:39:11 +0700

    Modified by www.SourceFiles.app

---
+++ beer-cau-gay-skin/lib/db.ts
@@ -0,0 +1,12 @@
+
+import { Pool } from 'pg'
+
+const globalForPool = global as unknown as { pgPool?: Pool }
+
+export const pool = globalForPool.pgPool || new Pool({
+  connectionString: process.env.SUPABASE_DB_URL,
+  max: 5,
+  idleTimeoutMillis: 10000
+})
+
+if (!globalForPool.pgPool) globalForPool.pgPool = pool
