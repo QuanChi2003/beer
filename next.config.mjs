9ac64da9ab6898a2a26bdebf5215b1feefeff357
QuanChi2003 <109083423+QuanChi2003@users.noreply.github.com>
2025-10-05 10:51:12 +0700

    Modified by www.SourceFiles.app

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  experimental: { serverActions: { allowedOrigins: ['*'] } },
-  images: { remotePatterns: [{ protocol: 'https', hostname: '**' }] }
-};
-export default nextConfig;
