abc38f50b41d54a7273bb361a2f26118e8935d80
QuanChi2003 <109083423+QuanChi2003@users.noreply.github.com>
2025-10-05 10:43:46 +0700

    Modified by www.SourceFiles.app

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+
+export default function Footer(){
+  return (
+    <footer className="mt-16 border-t bg-white">
+      <div className="container py-6 text-sm flex flex-col sm:flex-row justify-between">
+        <p><strong>Beer Cầu Gẫy</strong> · UX thân thiện · Chuẩn SEO · Mobile first</p>
+        <p className="text-neutral-500">cre:Quân</p>
+      </div>
+    </footer>
+  )
+}
