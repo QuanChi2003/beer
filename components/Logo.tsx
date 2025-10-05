0aae69f119e46a48cb76d3e07cbfc036c0209533
QuanChi2003 <109083423+QuanChi2003@users.noreply.github.com>
2025-10-05 21:14:05 +0700

    Modified by www.SourceFiles.app

--- components/Logo.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Logo({small=false}:{small?:boolean}){
-  return (
-    <div className={(small ? 'text-2xl' : 'text-4xl md:text-5xl') + ' font-extrabold'}>
-      🍺 Beer <span className="text-amber-500">Cầu Gẫy</span>
-    </div>
-  )
-}
